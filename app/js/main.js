96bcbabe71d55addafb6ab205fe671b7c631a26b
Ilya-S-Dev <ilyasmargunov@gmail.com>
2022-09-18 20:51:45 +0300

    added page new-items + adaptive to header and footer + popup(fancybox)

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $(function(){
 
+  $('.icon-th-list').on('click', () => {
+    $('.products-page__items .product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+$('.icon-th-large').on('click', () => {
+    $('.products-page__items .product__item').removeClass('list');
+    $('.icon-th-list').removeClass('active');
+    $('.icon-th-large').addClass('active');
+  });
+
+$('.js-range-slider').ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600
+  });
+
     $(".rate-star").rateYo({
         rating: 5,
         starWidth:"12px",
@@ -12,6 +32,15 @@ $(function(){
         slidesToShow:4,
         slidesToScroll:4
       });
+
+
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle('slow');
+});
+
 
 var mixer = mixitup('.products__inner-box');
 
